f78ef0b7db9ffcda7c8380d1d610988d5ca3d00b
LermanMax <lermanma@gmail.com>
2024-03-22 20:03:11 +0300

    update

--- tgbot/config.py
+++ tgbot/config.py
@@ -12,3 +12,5 @@ DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 DB_PORT = os.getenv('DB_PORT')
+
+RUN_FROM_CONTAINER = True if os.getenv('RUN_FROM_CONTAINER').lower() == 'true' else False
